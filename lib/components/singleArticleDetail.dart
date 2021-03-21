8f77ca28caebc8047d476f0829d7ecf791d12807
Yudi <singh.yudi10@gmail.com>
2021-03-21 23:04:55 +0530

    Added detailed view of a single article

---
+++ lib/components/singleArticleDetail.dart
@@ -0,0 +1,103 @@
+import 'package:demoapp/models/article.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SingleArticleDetail extends StatefulWidget {
+  final Article article;
+  SingleArticleDetail({this.article});
+  @override
+  _SingleArticleDetailState createState() => _SingleArticleDetailState();
+}
+
+class _SingleArticleDetailState extends State<SingleArticleDetail> {
+  void _launchURL() async => await canLaunch(widget.article.url)
+      ? await launch(widget.article.url)
+      : throw 'Could not launch ${widget.article.url}';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.article.title,
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              child: Image.network(
+                widget.article.urlToImage,
+                fit: BoxFit.fill,
+                errorBuilder: (BuildContext context, Object exception,
+                    StackTrace stackTrace) {
+                  print(exception);
+                  print(stackTrace);
+                  return Container(
+                    child: Image.network(
+                      "https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png",
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.article.title,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Merriweather-title"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.article.publishedAt,
+                style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Merriweather-title"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.article.description,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Merriweather-content"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.article.content,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Merriweather-content"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+                onPressed: _launchURL,
+                child: Text('Read the full Article here'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/components/singleArticleTile.dart
+++ lib/components/singleArticleTile.dart
@@ -1,3 +1,4 @@
+import 'package:demoapp/components/singleArticleDetail.dart';
 import 'package:demoapp/models/article.dart';
 import 'package:flutter/material.dart';
 
@@ -13,50 +14,59 @@ class _SingleArticleTileState extends State<SingleArticleTile> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 8.0),
-      child: Card(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                child: Image.network(
-                  widget.article.urlToImage,
-                  fit: BoxFit.fill,
-                  errorBuilder: (BuildContext context, Object exception,
-                      StackTrace stackTrace) {
-                    print(exception);
-                    print(stackTrace);
-                    return Container(
-                      child: Image.network(
-                        "https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png",
-                      ),
-                    );
-                  },
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      SingleArticleDetail(article: widget.article)));
+        },
+        child: Card(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.network(
+                    widget.article.urlToImage,
+                    fit: BoxFit.fill,
+                    errorBuilder: (BuildContext context, Object exception,
+                        StackTrace stackTrace) {
+                      print(exception);
+                      print(stackTrace);
+                      return Container(
+                        child: Image.network(
+                          "https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png",
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  widget.article.title,
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Merriweather-title"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.article.title,
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Merriweather-title"),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  widget.article.publishedAt,
-                  style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Merriweather-title"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.article.publishedAt,
+                    style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Merriweather-title"),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
